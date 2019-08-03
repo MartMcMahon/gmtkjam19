aa4b7c2c7540163a341b8c2e2713b8b024e426e5
James Robert <me@jiaaro.com>
2019-08-03 12:38:38 -0500

    board utils

--- board.js
+++ board.js
@@ -78,3 +78,38 @@ function find_unoccupied_cells(by_team) {
 function get_entity_count(of_team) {
 	return find_occupied_cells(of_team).length;
 }
+function get_cell_at_coords(x, y) {
+	return {
+		x: x, y: y,
+		tile: board.tiles[y][x],
+		entity: board.teams[y][x]
+	}
+}
+function get_cells_for_coords(coords) {
+	var cells = [];
+	for (var i=0; i < coords.length; i++) {
+		cells.push(get_cell_at_coords(coords[0], coords[1]));
+	}
+	return cells;
+}
+function get_adjacent_cell_coords(x, y) {
+	var coords = [];
+	for (var xi=-1; xi<=1; i++) {
+		for (var yi=-1; yi<=1; i++) {
+			// skip the cell we're searching around
+			if (xi == 0 && yi == 0) continue;
+			
+			// skip outside the map
+			if (x + xi < 0 || x + xi > board_size[0]
+				|| y + yi < 0 || y + yi > board_size[1]) {
+				continue
+			}
+			
+			coords.append([x + xi, y + yi]);
+		}
+	}
+	return coords;
+}
+function get_adjacent_cells(x, y) {
+	return get_cells_for_coords(get_adjacent_cell_coords(x, y))
+}

ba1d24b98fd2f6538e9ada84aeb43ef01daa7798
James Robert <me@jiaaro.com>
2019-08-03 14:13:24 -0500

    procedural generation

--- board.js
+++ board.js
@@ -1,5 +1,29 @@
 var board;
 
+function find_islands() {
+	var islands = [];
+	var cell, top, bottom, left, right;
+	for (var y=0; y<board_size[1];y++) {
+		for (var x=0; x<board_size[0];x++) {
+			cell = get_cell_at_coords(x, y);
+			
+			top = cell.at_offset(0, -1);
+			bottom = cell.at_offset(0, 1);
+			left = cell.at_offset(-1, 0);
+			right = cell.at_offset(1, 0);
+
+			if (	   (!top    || top.tile    !== cell.tile)
+					&& (!bottom || bottom.tile !== cell.tile)
+					&& (!left   || left.tile   !== cell.tile)
+					&& (!right  || right.tile  !== cell.tile)
+					) {
+				islands.push(cell)
+			}
+		}
+	}
+	return islands;
+}
+
 function init_board() {
 	board = {
 		tiles: [],
@@ -24,6 +48,12 @@ function init_board() {
 
 		}
 		board.teams.push(row);
+	}
+	var cell;
+	while (find_islands().length) {
+		cell = shuffle(find_islands())[0];
+		if      (cell.tile === tiles[0]) board.tiles[cell.y][cell.x] = tiles[1];
+		else if (cell.tile === tiles[1]) board.tiles[cell.y][cell.x] = tiles[0];
 	}
 }
 
@@ -82,7 +112,14 @@ function get_cell_at_coords(x, y) {
 	return {
 		x: x, y: y,
 		tile: board.tiles[y][x],
-		entity: board.teams[y][x]
+		entity: board.teams[y][x],
+		at_offset: function(dx, dy) {
+			var new_coords = get_coords_with_offset(x, y, dx, dy);
+			if (new_coords === null) {
+				return null;
+			}
+			return get_cell_at_coords.apply(null, new_coords)
+		}
 	}
 }
 function get_cells_for_coords(coords) {
@@ -92,24 +129,41 @@ function get_cells_for_coords(coords) {
 	}
 	return cells;
 }
-function get_adjacent_cell_coords(x, y) {
+function get_coords_with_offset(x, y, dx, dy) {
+	var newx = x + dx;
+	var newy = y + dy;
+	if (newx < 0 || newx >= board_size[0] || newy < 0 || newy >= board_size[1]) {
+		return null;
+	}
+	return [newx, newy];
+}
+function get_adjacent_cell(x, y, dx, dy) {
+	return get_cell_at_coords.apply(null, get_coords_with_offset(x, y, dx, dy))
+}
+function get_adjacent_cells_coords(x, y) {
 	var coords = [];
+	var point;
 	for (var xi=-1; xi<=1; i++) {
 		for (var yi=-1; yi<=1; i++) {
 			// skip the cell we're searching around
 			if (xi == 0 && yi == 0) continue;
 			
+			point = get_coords_with_offset(x, y, xi, yi);
+			
 			// skip outside the map
-			if (x + xi < 0 || x + xi > board_size[0]
-				|| y + yi < 0 || y + yi > board_size[1]) {
+			if (point === null) {
 				continue
 			}
 			
-			coords.append([x + xi, y + yi]);
+			coords.append(point);
 		}
 	}
 	return coords;
 }
 function get_adjacent_cells(x, y) {
-	return get_cells_for_coords(get_adjacent_cell_coords(x, y))
+	return get_cells_for_coords(get_adjacent_cells_coords(x, y))
+}
+function get_board_stats(board) {
+	var tiles = {};
+	var teams = {};
 }

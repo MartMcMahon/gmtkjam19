b8a5d9295036b61fe65990531de96e1f4133face
James Robert <me@jiaaro.com>
2019-08-04 10:23:39 -0500

    fast forward

--- event_handlers.js
+++ event_handlers.js
@@ -15,3 +15,43 @@ function spawn() {
 		add_random_creature(teams);
 	}
 }
+
+var gameInterval, animationTimeouts = [];
+function cancelAnimationTimeouts() {
+  for (var i=animationTimeouts.length;i--;) {
+  	clearTimeout(animationTimeouts[i])
+  }
+  animationTimeouts = [];
+}
+function stop_game() {
+  cancelAnimationTimeouts()
+  clearInterval(gameInterval);
+}
+function run_game(round_duration, animate) {
+	function go() {
+		cancelAnimationTimeouts();
+		if (animate !== false) {
+			animationTimeouts.push(
+				setTimeout(render_board, round_duration / 3)
+			);
+			animationTimeouts.push(
+				setTimeout(render_board, 2 * round_duration / 3)
+			);
+		}
+		game_tick();
+	}
+	
+	gameInterval = setInterval(go, round_duration);
+	if (animate !== false) {
+		go()
+	}
+}
+
+function fast_forward() {
+	stop_game();
+	run_game(ff_round_duration, false);
+}
+function normal_speed() {
+	stop_game();
+	run_game(round_duration);
+}

7c71f6c2c2fdf37297bd70ddea2ec8c49717e62b
Mart McMahon <themcman@gmail.com>
2019-08-04 13:48:48 -0500

    music working

--- sounds.js
+++ sounds.js
@@ -3,15 +3,22 @@ testSong.preload = "auto";
 
 var backgroundTracks = [];
 
-
 var src = document.createElement("source");
-src.src = "./sfx/re0.mp3";
+src.src = "./sfx/ambient.mp3";
 testSong.appendChild(src);
 testSong.load();
 
-function playMusic() {
+var musicIsPlaying = false;
+function toggleMusic() {
   testSong.currentTime = 0.01;
-  setTimeout(function() { testSong.play(); }, 1);
-  document.getElementById("music-button").innerText = "playing";
+  if (musicIsPlaying) {
+    testSong.volume = 0;
+    musicIsPlaying = false;
+    document.getElementById("music-button").innerText = "muted";
+  } else {
+    setTimeout(function() { testSong.play(); }, 1);
+    document.getElementById("music-button").innerText = "playing";
+    musicIsPlaying = true;
+  }
 }
 
